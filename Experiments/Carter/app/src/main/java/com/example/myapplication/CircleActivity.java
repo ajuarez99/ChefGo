f452fc6e98bc5747fb8219181af05e090b96941a
Carter Wunsch <cwunsch@iastate.edu>
2019-09-10 12:13:18 -0500

    created 4 activities for calculating different areas

---
+++ Experiments/Carter/app/src/main/java/com/example/myapplication/CircleActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CircleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circle);
+    }
+}
