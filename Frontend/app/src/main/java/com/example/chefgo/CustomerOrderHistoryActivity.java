1101b52acfcfe2c4e02fcc5607e2bbadeaf0846c
Carter Wunsch <cwunsch@iastate.edu>
2019-09-26 13:01:06 -0500

    added list view to customer order history

--- Frontend/app/src/main/java/com/example/chefgo/CustomerOrderHistoryActivity.java
+++ Frontend/app/src/main/java/com/example/chefgo/CustomerOrderHistoryActivity.java
@@ -3,12 +3,55 @@ package com.example.chefgo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView;
+
+import java.util.ArrayList;
 
 public class CustomerOrderHistoryActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_order_history);
+
+        listView = (ListView) findViewById(R.id.listview);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < 10; i++){
+            arrayList.add("Order #" + i);
+        }
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+
+
     }
+
+    public void displayList(){
+        listView = findViewById(R.id.listview);
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < 10; i++){
+            arrayList.add("Order #" + i);
+        }
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+
+            }
+
+        });
+    }
+
 }
