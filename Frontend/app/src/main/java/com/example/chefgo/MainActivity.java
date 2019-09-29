5dc49fbc3ca60ee42fe0cfb6cdc80eb265749eca
Joe Strobel <j.strobelight@yahoo.com>
2019-09-29 18:20:48 -0500

    Created customer profile activity.

--- Frontend/app/src/main/java/com/example/chefgo/MainActivity.java
+++ Frontend/app/src/main/java/com/example/chefgo/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.chefgo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nextButton = (Button) findViewById(R.id.buttonNext);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, CustomerProfileActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
