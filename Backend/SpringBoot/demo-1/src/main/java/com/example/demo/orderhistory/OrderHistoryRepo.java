cc50bcf672ea582e9cc5dbd067fa579986313495
ajuarez99 <allanjuarez86@gmail.com>
2019-09-29 18:48:50 -0500

    repos added

---
+++ Backend/SpringBoot/demo-1/src/main/java/com/example/demo/orderhistory/OrderHistoryRepo.java
@@ -0,0 +1,8 @@
+package com.example.demo.orderhistory;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface OrderHistoryRepo extends CrudRepository<OrderHistory, Integer> {
+
+}
