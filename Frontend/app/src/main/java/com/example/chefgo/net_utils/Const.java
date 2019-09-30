8891c22330e134613f24778b7e928cea85b5aceb
Carter Wunsch <cwunsch@iastate.edu>
2019-09-30 15:54:17 -0500

    deleted request activities

--- Frontend/app/src/main/java/com/example/chefgo/net_utils/Const.java
+++
@@ -1,8 +0,0 @@
-package com.example.chefgo.net_utils;
-
-public class Const {
-    public static final String URL_JSON_OBJECT = "http://coms-309-sb-3.misc.iastate.edu:8080/users";
-    public static final String URL_JSON_ARRAY = "http://coms-309-sb-3.misc.iastate.edu:8080/users";
-    public static final String URL_STRING_REQ = "http://coms-309-sb-3.misc.iastate.edu:8080/users";
-    public static final String URL_IMAGE = "https://api.android.info/volley/volley-image.jpeg";
-}
