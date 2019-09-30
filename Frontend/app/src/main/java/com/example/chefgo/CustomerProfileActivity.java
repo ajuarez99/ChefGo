22a1869985a47f4167fda5f2cd6878a5d4593b05
Joe Strobel <j.strobelight@yahoo.com>
2019-09-30 18:27:08 -0500

    json request should have an error

--- Frontend/app/src/main/java/com/example/chefgo/CustomerProfileActivity.java
+++ Frontend/app/src/main/java/com/example/chefgo/CustomerProfileActivity.java
@@ -2,13 +2,34 @@ package com.example.chefgo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.chefgo.net_utils.Const;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomerProfileActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_profile);
+
+
     }
+
 }
