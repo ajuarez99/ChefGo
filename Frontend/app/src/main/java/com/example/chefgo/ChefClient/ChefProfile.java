2c0796be05826b73819e5baf2bb290535e6528e3
Joe Strobel <j.strobelight@yahoo.com>
2019-10-14 16:14:56 -0500

    view is set up without listviews

---
+++ Frontend/app/src/main/java/com/example/chefgo/ChefClient/ChefProfile.java
@@ -0,0 +1,16 @@
+package com.example.chefgo.ChefClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chefgo.R;
+
+public class ChefProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_profile);
+    }
+}
