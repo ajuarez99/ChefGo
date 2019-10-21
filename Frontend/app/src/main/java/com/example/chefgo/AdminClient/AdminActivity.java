b1f562e884f2421700af4ab8cbba6311d2f34650
ajuarez <ajuarez@iastate.edu>
2019-10-21 22:36:44 +0000

    Resolve "Admin-Users"

--- Frontend/app/src/main/java/com/example/chefgo/AdminClient/AdminActivity.java
+++ Frontend/app/src/main/java/com/example/chefgo/AdminClient/AdminActivity.java
@@ -2,15 +2,30 @@ package com.example.chefgo.AdminClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.chefgo.CustomerProfileActivity;
+import com.example.chefgo.MainActivity;
 import com.example.chefgo.R;
 
 public class AdminActivity extends AppCompatActivity {
-
+    Button users;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+        users = findViewById(R.id.usersButton);
+        users.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent users = new Intent(AdminActivity.this, UsersActivity.class);
+
+                startActivity(users);
+            }
+        });
+
     }
 }
