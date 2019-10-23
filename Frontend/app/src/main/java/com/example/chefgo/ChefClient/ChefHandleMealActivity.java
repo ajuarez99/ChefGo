14a6cc37824b10378557d76dc8c1078dcb0a3b3b
Carter Wunsch <cwunsch@iastate.edu>
2019-10-22 23:55:46 -0500

    added accept and decline buttons to handle meal

--- Frontend/app/src/main/java/com/example/chefgo/ChefClient/ChefHandleMealActivity.java
+++ Frontend/app/src/main/java/com/example/chefgo/ChefClient/ChefHandleMealActivity.java
@@ -3,6 +3,8 @@ package com.example.chefgo.ChefClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,11 +14,29 @@ public class ChefHandleMealActivity extends AppCompatActivity {
 
     String itemDescription;
     TextView description;
+    Button acceptButton, declineButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chef_handle_meal);
         itemDescription = getIntent().getStringExtra("JSON_RESPONSE");
         description = findViewById(R.id.textView);
+        description.setText(itemDescription);
+
+        acceptButton = findViewById(R.id.buttonAccept);
+        declineButton = findViewById(R.id.buttonDecline);
+
+        acceptButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
+        declineButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
     }
 }
