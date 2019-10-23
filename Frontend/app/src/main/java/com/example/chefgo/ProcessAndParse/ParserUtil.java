a124bf282073299659a4943f985f3d19962dd21c
Joe Strobel <j.strobelight@yahoo.com>
2019-10-23 16:52:41 -0500

    I have added three mockito tests to ensure the ParserUtil is correctly parsing first and last name. The unimplemeted ProcessReviews class has been mocked in these tests.

--- Frontend/app/src/main/java/com/example/chefgo/ProcessAndParse/ParserUtil.java
+++ Frontend/app/src/main/java/com/example/chefgo/ProcessAndParse/ParserUtil.java
@@ -8,4 +8,14 @@ public class ParserUtil {
         return ret;
     }
 
+    public static String parseLastName(String name){
+        String reverse = "";
+        for (int i = name.length()-1; i > 0; i--){
+            reverse += "" + name.charAt(i);
+        }
+        int index = reverse.indexOf(" ");
+        int start = name.length() - index;
+        return name.substring(start);
+    }
+
 }
