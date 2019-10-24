ef52e896badea085a3c54f86475c8dec3edc934a
ajuarez99 <allanjuarez86@gmail.com>
2019-10-23 19:59:50 -0500

    get rid of useradapter

---
+++ Frontend/app/src/main/java/com/example/chefgo/MockitoTesting/LoginTesting.java
@@ -0,0 +1,23 @@
+package com.example.chefgo.MockitoTesting;
+
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import com.example.chefgo.R;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+@RunWith(AndroidJUnit4.class)
+public class LoginTesting {
+    @Test
+    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertTrue();
+    }
+}
+
+
