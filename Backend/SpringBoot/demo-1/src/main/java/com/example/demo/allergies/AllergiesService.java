a78a4d45a731db3dbf336534d14171b851f83e40
ajuarez99 <allanjuarez86@gmail.com>
2019-11-03 16:35:45 -0600

    extra java doc

--- Backend/SpringBoot/demo-1/src/main/java/com/example/demo/allergies/AllergiesService.java
+++ Backend/SpringBoot/demo-1/src/main/java/com/example/demo/allergies/AllergiesService.java
@@ -7,21 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.user.Users;
-
+/**
+ * @author SB_3
+ */
 @Service
 public class AllergiesService {
 
 	@Autowired
 	private AllergiesRepo allergiesRepo;
 	
-	
+	/**
+	 * gets all allergies
+	 * @return all allergies
+	 */
 	public List<Allergies> getAllAllergies() {
 		List<Allergies> allergies = new ArrayList<Allergies>();
 		
 		allergiesRepo.findAll().forEach(allergies :: add);
 		return allergies;
 	}
-	
+	/**
+	 * gets all allergies by a username
+	 * @param username
+	 * @return all allergies of the user
+	 */
 	public List<Allergies> getUserAllergies(String username){
 		List<Allergies> allergies = new ArrayList<Allergies>();
 		
@@ -29,11 +38,18 @@ public class AllergiesService {
 		return allergies;
 	}
 	
-	
+	/**
+	 * adds an allergy
+	 * @param allergy
+	 */
 	public void addAllergy(Allergies allergy) {
 		allergiesRepo.save(allergy);
 	}
-	
+	/**
+	 * assigns user to allergy
+	 * @param allergy
+	 * @param user
+	 */
 	public void assignUserToAllergy(Allergies allergy, Users user) {
 		allergy.setUser(user);
 		allergiesRepo.save(allergy);
