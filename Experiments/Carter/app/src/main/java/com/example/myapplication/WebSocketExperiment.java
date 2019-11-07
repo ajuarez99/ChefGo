bf140d6ba37e02986af8e33bf22196a6b147e531
Carter Wunsch <cwunsch@iastate.edu>
2019-11-06 19:28:44 -0600

    moved web socket experiment to my experiments folder

--- Experiments/Carter/app/src/main/java/com/example/myapplication/WebSocketExperiment.java
+++ Experiments/Carter/app/src/main/java/com/example/myapplication/WebSocketExperiment.java
@@ -1,11 +1,11 @@
-package com.example.chefgo;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.myapplication;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -19,42 +19,54 @@ public class WebSocketExperiment extends AppCompatActivity {
     private Button start;
     private TextView output;
     private OkHttpClient client;
+
     private final class EchoWebSocketListener extends WebSocketListener {
         private static final int NORMAL_CLOSURE_STATUS = 1000;
         @Override
         public void onOpen(WebSocket webSocket, Response response) {
             webSocket.send("This is a test");
-            webSocket.send("I wonder if it will work");
-            webSocket.send(ByteString.decodeHex("testing byte strings"));
+            webSocket.send("Are my web sockets working?");
+            webSocket.send(ByteString.decodeHex("deadbeef"));
             webSocket.close(NORMAL_CLOSURE_STATUS, "Exiting...");
         }
         @Override
         public void onMessage(WebSocket webSocket, String text) {
-            output("Receiving: " + text);
+            output("Receiving : " + text);
         }
         @Override
         public void onMessage(WebSocket webSocket, ByteString bytes) {
-            output("Receiving bytes: " + bytes.hex());
+            output("Receiving bytes : " + bytes.hex());
         }
         @Override
         public void onClosing(WebSocket webSocket, int code, String reason) {
             webSocket.close(NORMAL_CLOSURE_STATUS, null);
-            output("Closing: " + code + " / " + reason);
+            output("Closing : " + code + " / " + reason);
         }
         @Override
         public void onFailure(WebSocket webSocket, Throwable t, Response response) {
-            output("Error: " + t.getMessage());
+            output("Error : " + t.getMessage());
         }
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_socket_experiment);
-        start = findViewById(R.id.go);
+        start = findViewById(R.id.start);
         output = findViewById(R.id.output);
         client = new OkHttpClient();
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                start();
+            }
+        });
     }
-
+    private void start() {
+        Request request = new Request.Builder().url("ws://echo.websocket.org").build();
+        EchoWebSocketListener listener = new EchoWebSocketListener();
+        WebSocket ws = client.newWebSocket(request, listener);
+        client.dispatcher().executorService().shutdown();
+    }
     private void output(final String txt) {
         runOnUiThread(new Runnable() {
             @Override
