d09068dd6fbd2418bbe66dcf85804c2f302319ad
Carter Wunsch <cwunsch@iastate.edu>
2019-11-13 22:08:16 -0600

    added multiline edit text box and submit button

--- Frontend/app/src/main/java/com/example/chefgo/CustomerReviewOrder.java
+++ Frontend/app/src/main/java/com/example/chefgo/CustomerReviewOrder.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class CustomerReviewOrder extends AppCompatActivity {
 
     private String selectedOrder;
     private TextView reviewOrderTitle, orderDescriptionTitle, orderDescription;
+    private EditText reviewBox;
+    private Button submitReview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +23,11 @@ public class CustomerReviewOrder extends AppCompatActivity {
         selectedOrder = getIntent().getStringExtra("order");
 
         reviewOrderTitle = findViewById(R.id.reviewOrderTitle);
-
         orderDescriptionTitle = findViewById(R.id.orderDescriptionTitle);
-        orderDescriptionTitle.setPaintFlags(orderDescriptionTitle.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-
         orderDescription = findViewById(R.id.orderDescription);
+        reviewBox = findViewById(R.id.reviewBox);
+
+        orderDescriptionTitle.setPaintFlags(orderDescriptionTitle.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         orderDescription.setText(selectedOrder);
     }
 }
