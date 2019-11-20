3c15eade8ac706d8838ef61366e9893e226b106f
Joe Strobel <j.strobelight@yahoo.com>
2019-11-20 15:52:26 -0600

    Setup xml using relative layout for the page

---
+++ Frontend/app/src/main/java/com/example/chefgo/CustomerSeeChefProfile.java
@@ -0,0 +1,14 @@
+package com.example.chefgo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CustomerSeeChefProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_see_chef_profile);
+    }
+}
