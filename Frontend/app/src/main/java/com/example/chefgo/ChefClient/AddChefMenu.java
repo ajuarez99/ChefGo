5416d7f4cd6c367e8c492acb25ae50e27ed10892
ajuarez99 <allanjuarez86@gmail.com>
2019-12-08 20:21:44 -0600

    add chef request separated

--- Frontend/app/src/main/java/com/example/chefgo/ChefClient/AddChefMenu.java
+++ Frontend/app/src/main/java/com/example/chefgo/ChefClient/AddChefMenu.java
@@ -16,6 +16,7 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.chefgo.ChefClient.ChefRequests.AddChefRequests;
 import com.example.chefgo.DomainObjects.MenuDomain;
 import com.example.chefgo.DomainObjects.UsersDomain;
 import com.example.chefgo.R;
@@ -57,12 +58,12 @@ public class AddChefMenu extends AppCompatActivity {
         descText = findViewById(R.id.descText);
         costText = findViewById(R.id.costText);
         submitButton = findViewById(R.id.submitButton);
-
+        final Context ctx = this;
         //Set onClick listeners
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                postMenu(titleText.getText().toString(),
+                AddChefRequests.postMenu(URL, ctx,titleText.getText().toString(),
                         appsText.getText().toString(),
                         entreeText.getText().toString(),
                         dessertText.getText().toString(),
@@ -76,72 +77,6 @@ public class AddChefMenu extends AppCompatActivity {
         });
 
     }
-
-    private void postMenu(final String title,
-                          final String apps,
-                          final String entree,
-                          final String dessert,
-                          final String desc,
-                          final String cost,
-                          final UsersDomain theUser) {
-
-        if (title.equals("")) {
-            Toast.makeText(getApplicationContext(), "Enter a title bitch.", Toast.LENGTH_SHORT).show();
-            return;
-        } else if (apps.equals("")) {
-            Toast.makeText(getApplicationContext(), "Enter an app. N/A for none.", Toast.LENGTH_SHORT).show();
-            return;
-        } else if (entree.equals("")) {
-            Toast.makeText(getApplicationContext(), "Enter an entree. N/A for none.", Toast.LENGTH_SHORT).show();
-            return;
-        } else if (dessert.equals("")) {
-            Toast.makeText(getApplicationContext(), "Enter a dessert. N/A for none.", Toast.LENGTH_SHORT).show();
-            return;
-        } else if (desc.equals("")) {
-            Toast.makeText(getApplicationContext(), "Enter a description.", Toast.LENGTH_SHORT).show();
-            return;
-        } else if (cost.equals("")) {
-            Toast.makeText(getApplicationContext(), "Enter a price.", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        MenuDomain menu = new MenuDomain(title,apps,entree,dessert,Double.valueOf(cost),desc, theUser);
-
-        //Construct the user object for the menu object
-        Map<String, String> userMap =theUser.toJSON();
-
-        JSONObject userObject = new JSONObject(userMap);
-
-        //construct the menu object to be posted
-        Map<String, String> menuMap = menu.toJSON();
-        JSONObject menuObject = new JSONObject(menuMap);
-
-            try {
-                menuObject.put("chef", userObject);
-            } catch (JSONException e) {
-                e.printStackTrace();
-                Toast.makeText(getApplicationContext(),
-                        "Error: " + e.getMessage(),
-                        Toast.LENGTH_LONG).show();
-            }
-
-            JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, URL, menuObject, new Response.Listener<JSONObject>() {
-                @Override
-                public void onResponse(JSONObject response) {
-                    System.out.println(response.toString());
-                    Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_SHORT).show();
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    error.printStackTrace();
-                    Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
-                }
-            });
-
-            AppController.getInstance().addToRequestQueue(jsonRequest);
-        }
-
 
 
 
